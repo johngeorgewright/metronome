467d80dbdf6fa0539332cc37bce1419e714dc5f4
John Wright <johngeorge.wright@gmail.com>
2019-06-04 15:04:52 +0100

    feat: 🎸 add metronome marks

--- src/metronome.output.ts
+++ src/metronome.output.ts
@@ -1,15 +1,14 @@
-import Metronome from './metronome'
+import Metronome, { Note } from './metronome'
 import readLine from 'readline'
 
-const metronome = new Metronome({ bpm: 174, timeSignature: [4, 4] })
+const metronome = new Metronome({ bpm: 174, timeSignature: [6, Note.Eighth] })
 
-metronome.on('beat', beat => {
-  process.stdout.write(beat.toString())
-})
-
-metronome.on('bar.start', () => {
-  readLine.clearLine(process.stdout, -1)
-  readLine.moveCursor(process.stdout, -4, 0)
-})
-
-metronome.start()
+metronome
+  .on('beat', beat => {
+    process.stdout.write(beat.toString())
+  })
+  .on('bar.start', () => {
+    readLine.clearLine(process.stdout, -1)
+    readLine.moveCursor(process.stdout, -metronome.getTimeSignature()[0], 0)
+  })
+  .start()
--- src/metronome.ts
+++ src/metronome.ts
@@ -1,10 +1,28 @@
 import { EventEmitter } from 'events'
 import { bpmToMs } from './bpm'
 
-export type TimeSignature = [number, number]
+export enum Note {
+  Semibreve = 1,
+  Whole = Semibreve,
+
+  Minim = 2,
+  Half = Minim,
+
+  Crotchet = 4,
+  Quarter = Crotchet,
+
+  Quaver = 8,
+  Eighth = Quaver
+}
+
+export type TimeSignature = [
+  1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16,
+  Note
+]
 
 export interface MetronomeOptions {
   bpm: number
+  mark?: Note
   timeSignature: TimeSignature
 }
 
@@ -54,11 +72,13 @@ export default class Metronome extends EventEmitter {
   private beat: number = 1
   private bpm: number
   private intervalId: NodeJS.Timeout | void = void 0
+  private mark: Note
   private timeSignature: TimeSignature
 
-  constructor({ bpm, timeSignature }: MetronomeOptions) {
+  constructor({ bpm, mark = Note.Quarter, timeSignature }: MetronomeOptions) {
     super()
     this.bpm = bpm
+    this.mark = mark
     this.timeSignature = timeSignature
   }
 
@@ -80,26 +100,49 @@ export default class Metronome extends EventEmitter {
     }
   }
 
+  private intervalInMS(): number {
+    let ms = bpmToMs(this.bpm)
+
+    return this.timeSignature[1] === this.mark
+      ? ms
+      : this.timeSignature[1] > this.mark
+      ? ms / (this.timeSignature[1] - this.mark)
+      : ms * (this.mark - this.timeSignature[1])
+  }
+
   get running() {
     return !!this.intervalId
   }
 
+  getBPM(): number {
+    return this.bpm
+  }
+
   setBPM(bpm: number) {
     this.bpm = bpm
+    this.restart()
+  }
 
-    if (this.running) {
-      this.restart()
-    }
+  setTimeSignature(timeSignature: TimeSignature) {
+    this.timeSignature = timeSignature
+    this.restart()
+  }
+
+  getTimeSignature(): TimeSignature {
+    return this.timeSignature
   }
 
   restart() {
-    this.stop()
-    this.start()
+    if (this.running) {
+      this.stop()
+      this.start()
+    }
   }
 
   start() {
     this.emit('start')
-    this.intervalId = setInterval(this.tick, bpmToMs(this.bpm))
+    this.tick()
+    this.intervalId = setInterval(this.tick, this.intervalInMS())
   }
 
   stop() {
